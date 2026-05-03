10b8823dbc4f156967501d263c5c164146e9fa05
C <ba9309066@gmail.com>
2026-05-03 19:26:27 +0300

    Update Rectangle.java

--- src/Shapes/Rectangle.java
+++ src/Shapes/Rectangle.java
@@ -5,6 +5,7 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
+import java.awt.RenderingHints;
 
 public class Rectangle implements Shape{
 
@@ -23,18 +24,19 @@ public class Rectangle implements Shape{
     
 	@Override
 	public void draw(Graphics g) {
-		g.setColor(color);
-		int x = Math.min(startPoint.x, endPoint.x); /// i dont get it yet
+		
+		int x = Math.min(startPoint.x, endPoint.x); 
         int y = Math.min(startPoint.y, endPoint.y); 
-        
-//		int x = startPoint.x;
-//		int y = startPoint.y;
         
         int width = Math.abs(startPoint.x - endPoint.x);
         int height = Math.abs(startPoint.y - endPoint.y);
         
      	Graphics2D g2 = (Graphics2D) g;
-     	g2.setStroke(new BasicStroke(strokeSize));
+     	
+     	g2.setColor(color);
+     	g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+     	g2.setStroke(new BasicStroke(strokeSize, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND)); 
+     	
         if (filled) {
             g2.fillRect(x, y, width, height);  
         } else {
